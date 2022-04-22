3f58854a0020dae587da9d5f7c036bf2dd66113b
unknown <alan.wnek@gmail.com>
2022-04-22 23:45:46 +0200

    text fixes + favicon

--- js/checkIE.js
+++ js/checkIE.js
@@ -1,6 +1,6 @@
 var isIE = window.document.documentMode ? true : false;
 var ieMessage =
-	"<div style='padding: 16px;'><p>Ta strona nie wspiera przeglądarki Internet Explorer.</p><p>Proszę użyj normalnej przeglądarki internetowej.</p><p>Np. <a href='https://www.mozilla.org/pl/firefox/new/'>Firefox</a></p></div>";
+	"<div style='padding: 16px;'><p>Ta strona nie wspiera przeglądarki Internet Explorer.</p><p>Proszę użyj normalnej przeglądarki internetowej.</p><p>Np. <a href='https://www.google.com/intl/pl_pl/chrome/'>Chrome</a></p></div>";
 if (isIE) {
 	var body = document.getElementsByTagName("body");
 	body[0].innerHTML = ieMessage;
